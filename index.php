24d82bb6cc84b0e563a24d43d07bd3fc2bb8aaef
jay <jadersonrilidio@gmail.com>
2022-12-22 16:53:26 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+
+require_once('vendor/autoload.php');
+
+use \Book\ChapterOne\Example;
+
+$example = new Example();
+
---
+++ src/Example.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Book\ChapterOne;
+
+class Example
+{
+    private $hiddenVariable = true;
+    protected $protectedVariable = true;
+    public $publicVariable = true;
+
+    public function __debugInfo()
+    {
+        return call_user_func('get_object_vars', $this);
+    }
+}
